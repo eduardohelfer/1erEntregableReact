fd85ebe1cdcb9dedd5c83d2f991016d63d0295dc
Eduardo Helfer <eduardohelfer@gmx.net>
2022-09-19 15:20:42 -0500

    Second Commit

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -14,7 +14,7 @@ const QuoteBox = ({ randomQuote, randomColor, getRandomAll }) => {
 
   return (
     <article style={colorObj} className='card'>
-      <p className='card__quote'>{randomQuote.quote}</p>
+      <p className='card__quote'><span className='openQM'>‟</span>{randomQuote.quote}”</p>
 
       <h1 className='card__author'>{randomQuote.author}</h1>
       <button
